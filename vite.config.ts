989f9a794fd2a5febda51dd1bfd5c6363ef525d3
caltamar <49135789+caltamar@users.noreply.github.com>
2025-03-06 06:06:17 -0500

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "build", // Render busca la carpeta "build" en lugar de "dist"
+    chunkSizeWarningLimit: 1000, // Aumenta el límite de tamaño de chunk
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor"; // Agrupa dependencias en un solo archivo para optimizar carga
+          }
+        },
+      },
+    },
+  },
+});
